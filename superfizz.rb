5c442ee66614974623c653b088f6f440d646672b
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-01-22 22:18:11 -0700

    Modify superfizzbuzz

--- superfizz.rb
+++ superfizz.rb
@@ -2,19 +2,21 @@
 number = 0
 
 while number <= 1000
-  if number % 3 == 0 && number % 5 == 0 && number % 7 == 0 && number / 3 >=1
+  if number == 0
+    puts number
+  elsif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
     puts "SuperFizzBuzz"
-  elsif number % 3 == 0 && number % 7 == 0 && number / 3 >=1
+  elsif number % 3 == 0 && number % 7 == 0
     puts "SuperFizz"
-  elsif number % 5 == 0 && number % 7 == 0 && number / 3 >=1
+  elsif number % 5 == 0 && number % 7 == 0
     puts "SuperBuzz"
-  elsif number % 3 == 0 && number % 5 == 0 && number / 3 >=1
+  elsif number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
-  elsif number % 3 == 0 && number / 3 >=1
+  elsif number % 3 == 0
     puts "Fizz"
-  elsif number % 5 == 0 && number / 3 >=1
+  elsif number % 5 == 0
     puts "Buzz"
-  elsif number % 7 == 0 && number / 3 >=1
+  elsif number % 7 == 0
     puts "Super"
   else
     puts number
